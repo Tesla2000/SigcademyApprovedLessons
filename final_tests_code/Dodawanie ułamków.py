bb5eb2a54c84c73e501447a3e622e6db141e5755
Tesla2000 <fratajczak124@gmail.com>
2024-03-30 22:10:27 +0100

    Added Dodawanie ułamków.json lesson

---
+++ final_tests_code/Dodawanie ułamków.py
@@ -0,0 +1,48 @@
+import fractions
+import itertools
+import random
+import re
+
+
+def generate_questions() -> list[str]:
+    return [f"""<math xmlns="http://www.w3.org/1998/Math/MathML">
+      <mrow>
+        <mfrac>
+          <mrow>
+            <mn>{random.randint(2, 20)}</mn>
+          </mrow>
+          <mrow>
+            <mn>{random.randint(2, 20)}</mn>
+          </mrow>
+        </mfrac>
+        <mo>+</mo>
+        <mfrac>
+          <mrow>
+            <mn>{random.randint(2, 20)}</mn>
+          </mrow>
+          <mrow>
+            <mn>{random.randint(2, 20)}</mn>
+          </mrow>
+        </mfrac>
+      </mrow>
+    </math>"""
+            ]
+
+
+def generate_answers(question: str, answer: str, _: str) -> bool | str:
+    try:
+        numerator1, denominator1, numerator2, denominator2 = map(int, re.findall(r'<mn>(\d+)</mn>', question))
+        answer_numerator, answer_denominator = map(int, answer.split('/'))
+        return (fractions.Fraction(numerator1, denominator1) + fractions.Fraction(numerator2, denominator2)).numerator == answer_numerator and (fractions.Fraction(numerator1, denominator1) + fractions.Fraction(numerator2, denominator2)).denominator == answer_denominator
+    except Exception as e:
+        print(e)
+        return False
+
+
+if __name__ == '__main__':
+    random.seed(42)
+    questions = generate_questions()
+    answers = ['65/18']
+    for question, answer in itertools.zip_longest(questions, answers, fillvalue=''):
+        print(question)
+        assert generate_answers(question, answer, '')
